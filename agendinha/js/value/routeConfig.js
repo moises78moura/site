5e891b7a3ecbaa574c718f6b979499ae1f37dd14
moises78moura <moisesicdemoura@gmail.com>
2018-08-23 23:50:59 -0300

    o Merge no VS deu ruim, eu mandei alguns arquivos com erros, não consegui tratar corretamente.

--- agendinha/js/value/routeConfig.js
+++ agendinha/js/value/routeConfig.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-
-
 /**Testando um commit vindo do eclipse, vai dar merger */
-=======
 /**
  * Configuração de rotas
  */
->>>>>>> 5eb1f2362a7a9205cb4ec8f49750865a3f635411
 angular.module("appAgendinha").config(function($routeProvider){
     $routeProvider.when("/usuarios", {
         templateUrl: "view/usuarios.html",
