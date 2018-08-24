f46b34d0a3abc334354366f5a68da8bd2941036e
moises78moura <moisesicdemoura@gmail.com>
2018-08-24 00:06:32 -0300

    testeste

--- agendinha/js/value/configValue.js
+++ agendinha/js/value/configValue.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-/**Já havia feito uma alteração pelo eclipse */
-=======
 /**
  * Arquivo de configuração
  */
 
->>>>>>> 5eb1f2362a7a9205cb4ec8f49750865a3f635411
 angular.module("appAgendinha").value("config",{
     baseUrl:"http://localhost:8080"
 });
